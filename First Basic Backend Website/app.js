6180f0a793fe8cf2608b2c5f30d6c838fa8ae2bb
Neeraj Kumar <nk7075034@gmail.com>
2022-06-06 13:58:29 +0530

    Made params global

--- First Basic Backend Website/app.js
+++ First Basic Backend Website/app.js
@@ -13,9 +13,9 @@ app.set("views", path.join(__dirname, "templates"));
 app.use(express.urlencoded()); // Middleware to parse submitted form data
 
 // Endpoints
+const params = { "title": "Extreme Gym", "message": "Welcome to the Extreme Gym", "promotion": "Get our premium subscription only at $50" };
 app.get("/", (req, res) => {
     console.log(`Endpoint hitted = ${req.url}`);
-    const params = { "title": "Extreme Gym", "message": "Welcome to the Extreme Gym", "promotion": "Get our premium subscription only at $50" };
     res.status(200).render("template1.pug", params);
 });
 
@@ -23,7 +23,6 @@ app.post("/", (req, res) => {
     fs.appendFile("Form data.txt", JSON.stringify(req.body) + "\n", () => {
         console.log("Data had been saved successfully");
     });
-    const params = { "title": "Extreme Gym", "message": "Welcome to the Extreme Gym", "promotion": "Get our premium subscription only at $50" };
     res.status(200).render("template1.pug", params);
 });
 
